919a60a8974aff04084f62ba7d547bd7bd8febfc
sudha sahu <sudhasahu0535@gmail.com>
2025-06-03 18:02:11 +0530

    End of section11

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -5,10 +5,11 @@ import { MembersService } from '../../_services/members.service';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { PhotoEditorComponent } from "../photo-editor/photo-editor.component";
 
 @Component({
   selector: 'app-member-edit',
-  imports: [TabsModule, FormsModule],
+  imports: [TabsModule, FormsModule, PhotoEditorComponent],
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.css'
 })
@@ -45,6 +46,9 @@ export class MemberEditComponent {
         this.editForm?.reset(this.member);
       }
     })
-    
+  }
+
+  onMemberChange(event:Member){
+    this.member = event;
   }
 }
